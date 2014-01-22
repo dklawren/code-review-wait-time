444a919f8daad3595d7dab08d5d650c2d0c09ea0
Rick Eyre <rick.eyre@hotmail.com>
2014-01-22 13:53:09 -0500

    Fix bad string...

--- js/es-glue.js
+++ js/es-glue.js
@@ -6,12 +6,12 @@ define(["jquery"], function($) {
 
   ESGlue.query = function(options, onDone) {
     $.get("http://10.251.33.248:9200/reviews/review",
-      '{
-        "from":"reviews",
-        "select":{"name":"num_bugs","value":"bug_id","aggregate":"count"},
-        "edges":["product","component"],
-        "esfilter":{"term":{"product":"firefox"}}
-       }', onDone, "json");
+      '{' + 
+        '"from":"reviews",' +
+        '"select":{"name":"num_bugs","value":"bug_id","aggregate":"count"},' +
+        '"edges":["product","component"],' +
+        '"esfilter":{"term":{"product":"firefox"}}' +
+       '}', onDone, "json");
   };
 
   return ESGlue;
