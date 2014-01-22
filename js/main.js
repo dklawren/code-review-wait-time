2b79224549f9c228900a05cd460c67b163e2960c
Ali Al Dallal <ali@alicoding.com>
2014-01-22 14:56:35 -0800

    Add return

--- js/main.js
+++ js/main.js
@@ -99,21 +99,21 @@ require(['jquery', 'es-glue', 'd3'], function ($, ES, d3) {
     var row = d3.select("#component-times")
         .selectAll("tr")
         .data(Object.keys(aggregate)
-                    .filter(function(k){ aggregate[k].year.count}))
+                    .filter(function(k){ return aggregate[k].year.count}))
         .enter()
         .append("tr");
     row.append("td")
-        .text(function(d){ capitalize(d.split("|")[1])});
+        .text(function(d){ return capitalize(d.split("|")[1])});
     row.append("td")
-        .text(function(d){ formatTime(aggregate[d].week.time / aggregate[d].week.count)});
+        .text(function(d){ return formatTime(aggregate[d].week.time / aggregate[d].week.count)});
     row.append("td")
-        .text(function(d){ formatTime(aggregate[d].month.time / aggregate[d].month.count)});
+        .text(function(d){ return formatTime(aggregate[d].month.time / aggregate[d].month.count)});
     row.append("td")
-        .text(function(d){ formatTime(aggregate[d].year.time / aggregate[d].year.count)});
+        .text(function(d){ return formatTime(aggregate[d].year.time / aggregate[d].year.count)});
     row.append("td")
-        .text(function(d){ formatTime(aggregate[d].year.min)});
+        .text(function(d){ return formatTime(aggregate[d].year.min)});
     row.append("td")
-        .text(function(d){ formatTime(aggregate[d].year.max)});
+        .text(function(d){ return formatTime(aggregate[d].year.max)});
 
     var data1 = [];
     for (var i = 0; i < 14; i += 0.5) {
