6ee1d57c2b4aa416ad6ab1f33331a480b701d85c
David Lawrence <dklawren@hotmail.com>
2014-01-22 17:48:55 -0500

    fixed variable collision

--- js/main.js
+++ js/main.js
@@ -115,12 +115,12 @@ require(['jquery', 'es-glue', 'd3'], function ($, ES, d3) {
     row.append("td")
         .text(function(d) formatTime(aggregate[d].year.max));
 
-    var d1 = [];
+    var data1 = [];
     for (var i = 0; i < 14; i += 0.5) {
-      d1.push([i, Math.sin(i)]);
+      data1.push([i, Math.sin(i)]);
     }
-    var d2 = [[0, 3], [4, 8], [8, 5], [9, 13]];
-    var d3 = [[0, 12], [7, 12], null, [7, 2.5], [12, 2.5]];
-    $.plot("#chart", [ d1, d2, d3 ]);
+    var data2 = [[0, 3], [4, 8], [8, 5], [9, 13]];
+    var data3 = [[0, 12], [7, 12], null, [7, 2.5], [12, 2.5]];
+    $.plot("#chart", [ data1, data2, data3 ]);
   });
 });
